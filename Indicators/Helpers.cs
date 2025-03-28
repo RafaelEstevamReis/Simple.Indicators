fc20575a812312651d7d6b241e0d0c5b17a7a5f0
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2025-03-27 23:05:12 -0300

    Create Helpers.cs

---
+++ Indicators/Helpers.cs
@@ -0,0 +1,23 @@
+﻿namespace Indicators;
+
+using System;
+
+public class Helpers
+{
+    public static (int, int) GetValueIndexes(int startYear, decimal[][] table, DateTime dtRef)
+    {
+        var ixYear = dtRef.Year - startYear;
+        var ixMonth = dtRef.Month - 1;
+        return (ixYear, ixMonth);
+    }
+    public static bool IsValidIndexes(int ixYear, int ixMonth, decimal[][] table)
+    {
+        if (ixYear < 0) return false;
+        if (ixYear >= table.Length) return false;
+
+        if (ixMonth < 0) return false;
+        if (ixMonth >= table[ixYear].Length) return false;
+
+        return true;
+    }
+}
