2d0b540793ead23e86444a4a22d950abb7ca7b23
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2026-01-24 17:57:11 -0300

    Create TableHelper.cs

---
+++ Indicators/TableHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace Simple.Indicators;
+
+using System;
+using System.Linq;
+
+public static class TableHelper
+{
+    static ITable[]? allTables = null;
+
+    private static void initializeTableNames()
+    {
+        var tInterface = typeof(ITable);
+        var asm = tInterface.Assembly;
+
+        var allTableTypes = asm.GetTypes()
+                               .Where(tInterface.IsAssignableFrom)
+                               .Where(t => t.IsClass && !t.IsInterface)
+                               .ToArray();
+
+        allTables = allTableTypes.Select(t => (ITable)Activator.CreateInstance(t)).ToArray();
+    }
+    public static ITable[] GetAllTables()
+    {
+        if (allTables == null) initializeTableNames();
+
+        return allTables!;
+    }
+    public static ITable? GetTableByName(string name)
+    {
+        if (allTables == null) initializeTableNames();
+
+        return allTables.FirstOrDefault(t => t.GetType().FullName == name);
+    }
+
+}
